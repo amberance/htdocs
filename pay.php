1455870447e34dfbca7c31be8a0d5ee36d02c521
Danger Dan <me@me.com>
2022-10-07 18:45:59 -0400

    pay php file added

---
+++ pay.php
@@ -0,0 +1,267 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<meta name="viewport"
+	content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1">
+<title>Payments</title>
+<!-- Primary Meta Tags -->
+<script id="boxcoin" src="https://www.radient.one/boxcoin/js/client.js"></script>
+<!-- Open Graph / Facebook -->
+
+
+
+
+<meta name="theme-color" content="#f65169">
+<link rel="shortcut icon" type="image/png"
+	href="https://www.radient.one/img/2022-03-16/image_1647408763070005900_1.png"
+	sizes="128x128">
+<link rel="manifest" href="https://www.radient.one/src/manifest.json">
+<link rel="apple-touch-icon"
+	href="https://www.radient.one/src/192x192.png">
+<link rel="stylesheet" type="text/css"
+	href="https://www.radient.one/themes/default/css/style.css?v=g2111113111411111322111242111111111111111111111111s1131211111111111113.6.8">
+<link rel="stylesheet" type="text/css"
+	href="https://www.radient.one/themes/default/css/lightGallery/lightgallery.css">
+<link rel="stylesheet" type="text/css"
+	href="https://www.radient.one/themes/default/css/videojscss/video-js.css">
+<link rel="stylesheet" type="text/css"
+	href="https://www.radient.one/themes/default/css/checkbox/checkbox.css">
+<link rel="stylesheet" type="text/css"
+	href="https://www.radient.one/themes/default/css/crop/cropmain.css">
+<link rel="stylesheet" type="text/css"
+	href="https://www.radient.one/themes/default/css/swiper/swiper-bundle.css">
+
+<link rel="stylesheet" type="text/css"
+	href="https://www.radient.one/themes/default/css/audioplayer.css?v=m11">
+<script type="text/javascript"
+	src="https://www.radient.one/themes/default/js/jquery-v3.5.1.min.js"></script>
+<script type="text/javascript"
+	src="https://www.radient.one/themes/default/js/jquery.form.js"></script>
+<script type="text/javascript"
+	src="https://www.radient.one/themes/default/js/share.js?v=13.6.8"></script>
+<script type="text/javascript"
+	src="https://www.radient.one/themes/default/js/clipboard/clipboard.min.js"></script>
+<script type="text/javascript"
+	src="https://www.radient.one/themes/default/js/lightGallery/lightgallery-all.min.js"></script>
+<script type="text/javascript"
+	src="https://www.radient.one/themes/default/js/autoresize.min.js"></script>
+<script type="text/javascript"
+	src="https://www.radient.one/themes/default/js/videojs/video.js"></script>
+<script type="text/javascript"
+	src="https://www.radient.one/themes/default/js/scrollBar/jquery.slimscroll.min.js"></script>
+<script type="text/javascript">
+var siteurl = 'https://www.radient.one/';
+$(function() {
+    $('.commenta').autoResize();
+    var clipboard = new ClipboardJS('.copyUrl');
+});
+    </script>
+<script type="text/javascript"
+	src="https://www.radient.one/themes/default/js/inora.js?v=o51171113.6.8"></script>
+<script type="text/javascript"
+	src="https://www.radient.one/themes/default/js/character_count.js?v=3.6.8"></script>
+
+<script type="text/javascript">
+        </script>
+<script type="text/javascript"
+	src="https://www.radient.one/themes/default/js/swiper/swiper-bundle.min.js"></script>
+</head>
+<body>
+	<div class="header">
+		<div class="i_header_in">
+			<div class="i_logo tabing flex_">
+				<a href="https://www.radient.one/"><img
+					src="https://www.radient.one/img/2022-03-16/image_1647408798008302700_1.png"></a>
+			</div>
+			<div class="i_search" style="position: relative;">
+				<div class="mobile_back tabing flex_ mobile_srcbtn">
+					<div class="i_header_btn_item transition">
+						<div class="i_h_in is_mobile">
+							<svg id="Capa_102" data-name="Capa 101"
+								xmlns="http://www.w3.org/2000/svg" viewBox="0 0 447.24 383.56">
+								<path
+									d="M420.36,192.23a31.07,31.07,0,0,0-5.53-.41H99.31l6.87-3.2a63.87,63.87,0,0,0,18.08-12.8l88.48-88.48a33.1,33.1,0,0,0,4.64-42.4A32,32,0,0,0,172.66,38a33.26,33.26,0,0,0-3.28,2.76l-160,160a32,32,0,0,0,0,45.25l0,0,160,160a32,32,0,0,0,45.26-.06,32.59,32.59,0,0,0,2.74-3.14,33.1,33.1,0,0,0-4.64-42.4l-88.32-88.64a63.88,63.88,0,0,0-16-11.68l-9.6-4.32H413.07a33.13,33.13,0,0,0,33.76-26.88A32,32,0,0,0,420.36,192.23Z"
+									transform="translate(0 -31.84)" /></svg>
+						</div>
+					</div>
+				</div>
+				<input type="text" class="i_s_input" id="search_creator"
+					placeholder="Search Creators">
+				<!---->
+				<div class="i_general_box_search_container generalBox"
+					style="min-width: 360px; display: none; max-height: 360px !important;">
+					<div class="btest">
+						<div class="i_user_details">
+							<div class="i_box_messages_header">Search</div>
+							<div class="i_header_others_box sb_items"></div>
+						</div>
+						<!---->
+						<!---->
+					</div>
+				</div>
+				<!---->
+			</div>
+			<div class="i_header_right">
+				<div class="i_one">
+					<div
+						class="i_header_btn_item transition search_mobile mobile_srcbtn">
+						<div class="i_h_in">
+							<svg id="Capa_101" data-name="Capa 101"
+								xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512.54 511.26">
+								<path
+									d="M481.28,377,409.6,323.2c17.92-30.72,25.6-66.56,25.6-105a217.6,217.6,0,1,0-217.6,217.6c38.4,0,71.68-10.24,105-25.6l53.76,71.68c28.16,35.84,81.92,41,115.2,7.68C522.24,458.88,519.68,405.12,481.28,377ZM217.6,384.64A166.4,166.4,0,1,1,384,218.24,166.07,166.07,0,0,1,217.6,384.64Z"
+									transform="translate(0 -0.64)" /></svg>
+						</div>
+					</div>
+					<div class="i_header_btn_item topPoints transition ownTooltip"
+						data-label="Get Point/Point Balance" id="topPoints"
+						data-type="topPoints">
+						<div class="i_h_in">
+							<svg id="Capa_40" data-name="Capa 40"
+								xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 438.29">
+								<polygon points="393.06 93.2 393.06 0 293.87 0 393.06 93.2" />
+								<polygon
+									points="141.81 112.87 370.19 112.87 256 5.58 141.81 112.87" />
+								<polygon
+									points="385.13 142.87 126.87 142.87 256 438.29 385.13 142.87" />
+								<polygon points="118.94 93.2 218.13 0 118.94 0 118.94 93.2" />
+								<polygon
+									points="88.94 112.87 88.94 22.59 3.25 112.87 88.94 112.87" />
+								<polygon
+									points="423.06 112.87 508.75 112.87 423.06 22.59 423.06 112.87" />
+								<polygon
+									points="417.88 142.87 312.55 383.82 512 142.87 417.88 142.87" />
+								<polygon
+									points="94.13 142.87 0 142.87 199.45 383.82 94.13 142.87" /></svg>
+						</div>
+					</div>
+					<div class="i_header_btn_item transition shopi ownTooltip"
+						data-label="Marketplace">
+						<a href="https://www.radient.one/marketplace?cat=all">
+							<div class="i_h_in">
+								<svg id="Layer_1" data-name="Layer 1"
+									xmlns="http://www.w3.org/2000/svg" viewBox="0 0 300 300.01">
+									<path
+										d="M150,0A150,150,0,1,0,300,150,150,150,0,0,0,150,0Zm82.17,222.64h0a14.74,14.74,0,0,1-14.74,14.74h-9.11V173.62H165.25v63.76H81.08a14.74,14.74,0,0,1-14.73-14.74V146.17a28.71,28.71,0,0,0,18.75-9,28.75,28.75,0,0,0,42,0,28.75,28.75,0,0,0,42,0,28.75,28.75,0,0,0,42,0,28.71,28.71,0,0,0,21,9.13v76.35Zm0-91.91A13.26,13.26,0,0,1,219,117.49a7.78,7.78,0,1,0-15.56,0,13.24,13.24,0,1,1-26.47,0,7.78,7.78,0,0,0-15.56,0,13.24,13.24,0,1,1-26.47,0,7.78,7.78,0,1,0-15.56,0,13.23,13.23,0,1,1-26.46,0,7.79,7.79,0,0,0-15.57,0,13.23,13.23,0,0,1-26.26,2.3L91.21,65.35H207.73l37.59,53.76A13.24,13.24,0,0,1,232.2,130.73Z"
+										transform="translate(0)" />
+									<rect x="89.04" y="174.16" width="47.62" height="33.26" /></svg>
+							</div>
+						</a>
+					</div>
+					<div class="i_header_btn_item topMessages transition ownTooltip"
+						data-label="Messenger" id="topMessages" data-type="topMessages">
+						<div class="i_h_in">
+							<div class="i_notifications_count msg_not" style="display: none;">
+								<div class="isum sum_m" data-id="">
+									<br /> <b>Notice</b>: Undefined variable:
+									totalMessageNotifications in <b>C:\xampp\htdocs\themes\default\layouts\header\header.php</b>
+									on line <b>62</b><br />
+								</div>
+							</div>
+							<svg id="Capa_38" data-name="Capa 38"
+								xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+								<path
+									d="M256,0C114.62,0,0,106.11,0,237c0,74.6,37.22,141.12,95.39,184.58V512l87.17-47.84A275.35,275.35,0,0,0,256,474.08c141.38,0,256-106.11,256-237S397.38,0,256,0Zm25.44,319.2-65.18-69.54L89.06,319.2,229,170.66l66.79,69.53,125.6-69.53Z" /></svg>
+						</div>
+					</div>
+					<div
+						class="i_header_btn_item topNotifications transition ownTooltip"
+						data-label="Notifications" id="topNotifications"
+						data-type="topNotifications">
+						<div class="i_h_in">
+							<div class="i_notifications_count not_not" style="display: none;">
+								<div class="isum sum_not" data-id="">
+									<br /> <b>Notice</b>: Undefined variable: totalNotifications in
+									<b>C:\xampp\htdocs\themes\default\layouts\header\header.php</b>
+									on line <b>68</b><br />
+								</div>
+							</div>
+							<svg id="Capa_37" data-name="Capa 37"
+								xmlns="http://www.w3.org/2000/svg" viewBox="0 0 41.75 46.24">
+								<path
+									d="M38.3,27.18v-9.4a15.18,15.18,0,0,0-9.5-14h.06a6.27,6.27,0,0,0-11.48,0h.05a15.18,15.18,0,0,0-9.49,14v9.4A5.77,5.77,0,0,0,2.24,33v1.25A5.78,5.78,0,0,0,8,40h30.2A5.78,5.78,0,0,0,44,34.22V33A5.78,5.78,0,0,0,38.3,27.18Z"
+									transform="translate(-2.24)" />
+								<path
+									d="M17.57,46.24a6.3,6.3,0,0,0,5.74-3.75H11.83A6.29,6.29,0,0,0,17.57,46.24Z"
+									transform="translate(-2.24)" /></svg>
+						</div>
+					</div>
+					<div class="i_header_btn_item getMenu transition" id="topMenu"
+						data-type="topMenu">
+						<div class="i_h_in">
+							<svg id="Capa_36" data-name="Capa 36"
+								xmlns="http://www.w3.org/2000/svg" viewBox="0 0 292.36 164.46">
+								<path
+									d="M286.93,69.38A17.52,17.52,0,0,0,274.09,64H18.27A17.56,17.56,0,0,0,5.42,69.38a17.93,17.93,0,0,0,0,25.69L133.33,223a17.92,17.92,0,0,0,25.7,0L286.93,95.07a17.91,17.91,0,0,0,0-25.69Z"
+									transform="translate(0 -63.95)" /></svg>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+
+	<script type="text/javascript">
+                </script>
+	<div class="wrapper bCreatorBg">
+		<div class="i_not_found_page transition">
+                
+                
+                
+<?php
+include_once "includes/connect.php";
+
+$price = 10;
+
+try {
+
+    $orderId = filter_input(INPUT_GET, 'order_id', FILTER_SANITIZE_URL);
+    $creditPlan = filter_input(INPUT_GET, 'creditPlan', FILTER_SANITIZE_URL);
+    $customerId = - 1;
+    $productId = - 1;
+    $productPrice = - 1;
+    foreach ($db->query("SELECT * FROM i_user_payments where order_key='$orderId'") as $row) {
+        $customerId = $row['payer_iuid_fk'];
+        $productId = $row['paymet_product_id'];
+        $creditPlanId = $row['credit_plan_id'];
+    }
+
+    if (! empty($productId)) {
+        foreach ($db->query("SELECT * FROM i_user_product_posts where pr_id='$productId'") as $row) {
+            $productPrice = $row['pr_price'];
+        }
+    } else if (! empty($creditPlanId)) {
+        foreach ($db->query("SELECT * FROM i_premium_plans where plan_id='$creditPlanId'") as $row) {
+            $productPrice = $row['amount'];
+        }
+    }
+    echo "</ol>";
+} catch (PDOException $e) {
+    print "Error!: " . $e->getMessage() . "<br/>";
+    die();
+}
+
+?>
+
+<center>
+	Please pay the specified amount to the given address through your
+	crypto wallet.<br />
+
+	<div
+		data-boxcoin="custom-<?php echo filter_input(INPUT_GET, 'order_id', FILTER_SANITIZE_URL) ?>-<?php echo $customerId ?>"
+		data-price="<?php echo $productPrice ?>"
+		data-external-reference="<?php echo filter_input(INPUT_GET, 'order_id', FILTER_SANITIZE_URL) ?>"
+		data-title="<?php echo filter_input(INPUT_GET, 'item_id', FILTER_SANITIZE_URL) ?>"
+		data-description="<?php echo $customerId ?>"></div>
+	Please do not close your browser until the transaction finished.
+</center>
+
+
+
+
+		</div>
+	</div>
+</body>
+</html>
