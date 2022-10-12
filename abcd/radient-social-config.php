93562dbcaa9afa1827dcf72364730683ef73f774
Darcy DeClute <96313181+scrumtuous@users.noreply.github.com>
2022-10-12 12:22:41 -0400

    Create radient-social-config.php

---
+++ abcd/radient-social-config.php
@@ -0,0 +1,10 @@
+<?php
+define('BXC_USER', 'Admin');
+define('BXC_PASSWORD', '$2y$10$baEJhu5n/hPsjeopwzXp1uMFk6UqRwLkNhgeSwY1ySu.Jxm93mj5e');
+define('BXC_DB_NAME', 'radient_boxcar');
+define('BXC_DB_USER', 'radient_willy');
+define('BXC_DB_PASSWORD', 'PaladinB$U@***');
+define('BXC_DB_HOST', 'localhost');
+define('BXC_DB_PORT', '3306');
+define('BXC_URL', 'https://www.radient.social/abcd/');
+?>
